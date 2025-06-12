ceda500a357946f10d8f4370d703d9e8161236b3
liya-codes <liyathequeen1@gmail.com>
2025-06-12 15:52:59 +0300

    add health route to app install requests for pytest

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routes import secret
+from app.routes import secret, health
 
 app = FastAPI()
 
 # app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(secret.router)
+app.include_router(health.router)
 
 
 @app.get("/")

b0ffaaa7d3459ecf2737d1869dec42f2cbc4ad60
liya-codes <liyathequeen1@gmail.com>
2025-06-12 16:05:23 +0300

    combine the test files to 1 test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,12 @@ def test_health():
 
     response = requests.get(url, timeout=5)      # 5-second safety timeout
     assert response.status_code == 200
+
+
+
+def test_health():
+    port = os.getenv("PORT", "5000")             # uses $PORT or defaults to 5000
+    url = f"http://localhost:{port}/health"
+
+    response = requests.get(url, timeout=5)      # 5-second safety timeout
+    assert response.status_code == 200
--- tests/test_secret.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def test_secret():
-    port = os.getenv("PORT", "5000")             # uses $PORT or defaults to 5000
-    url = f"http://localhost:{port}/secret"
-
-    response = requests.get(url, timeout=5)      # 5-second safety timeout
-    assert response.status_code == 200
